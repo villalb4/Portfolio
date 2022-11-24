d8a4a0323a2407469796b529acfab233b001f146
villalb4 <villalb4angel@gmail.com>
2022-11-23 23:27:13 -0300

    change: import main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header';
+import Home from './views/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Home />
     </div>
   );
 }
