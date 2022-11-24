58d38e0b2f47a690a7b0b1bc637af095dd5df4f1
villalb4 <villalb4angel@gmail.com>
2022-11-23 23:24:33 -0300

    add: views folder

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section>
+          <h1>FULL STACK DEVELOPER</h1>
+          <p>A.VILLALBA</p>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Home
